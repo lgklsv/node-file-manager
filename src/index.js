b9cd06019d06a1d07bd6898089cf1879f01f98fc
lgklsv <kolosov20011@gmail.com>
2023-06-23 11:30:27 +0300

    feat: display entered in process args  username

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { createArgsMap } from './utils/createArgsMap.js';
+
+const processArgsMap = createArgsMap();
+let username = processArgsMap?.username || 'Anonymous user';
+
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = readline.createInterface({ input, output });
