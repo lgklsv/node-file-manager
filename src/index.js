dc6d66e4cab28f9ca16242d975fac5b5d9ea8e38
lgklsv <kolosov20011@gmail.com>
2023-06-23 11:53:48 +0300

    feat: add exit messages on cmp + c and .exit commands

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ let username = processArgsMap?.username || 'Anonymous user';
 console.log(`Welcome to the File Manager, ${username}!`);
 
 const rl = readline.createInterface({ input, output });
+
+rl.on('line', (input) => {
+  if (input === '.exit') {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    rl.close();
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  rl.close();
+});
