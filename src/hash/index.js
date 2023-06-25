8ad6246b06eb6ce7bf04a3a67277a4a66032019e
lgklsv <kolosov20011@gmail.com>
2023-06-26 00:00:33 +0300

    refactor: change hash function to use streams

--- src/hash/index.js
+++ src/hash/index.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 import crypto from 'node:crypto';
 
 import { extractPath } from '../utils/extractPath.js';
@@ -7,12 +7,19 @@ import { ERROR_MES } from '../const/errors.js';
 export const generateHash = async (input) => {
   try {
     const filePath = extractPath(input);
-    const fileBuffer = await fs.readFile(filePath);
+    const readable = fs.createReadStream(filePath);
     const hashSum = crypto.createHash('sha256');
-    hashSum.update(fileBuffer);
 
-    const hex = hashSum.digest('hex');
-    console.log(hex);
+    await new Promise((resolve, reject) => {
+      readable.pipe(hashSum.setEncoding('hex')).pipe(process.stdout);
+      readable.on('error', (err) => {
+        reject(err);
+      });
+      readable.on('close', () => {
+        console.log('\t');
+        resolve();
+      });
+    });
   } catch {
     console.log(ERROR_MES.operation);
   }
